8260e24d2b180c453adc6506b2cb8d7c7897cfb9
logan <loganfarseer@gmail.com>
2025-01-30 01:27:31 -0800

    'sync'

--- user.ts
+++ user.ts
@@ -2,8 +2,24 @@
 
 import { prisma } from './prisma'
 
+/**
+ * Find a user by their email.
+ * @param email - The email of the user to find.
+ * @returns The user object if found, otherwise null.
+ */
 export const userFindByEmail = async (email: string) => {
   return await prisma.user.findUnique({
     where: { email },
+  })
+}
+
+/**
+ * Find a user by their ID.
+ * @param id - The ID of the user to find.
+ * @returns The user object if found, otherwise null.
+ */
+export const userFindById = async (id: string) => {
+  return await prisma.user.findUnique({
+    where: { id },
   })
 }
